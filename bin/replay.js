803318ae94e9cdb0d1345ec6fd009f78c95b331e
Jake McKown <jbmckown@gmail.com>
2019-07-27 20:25:23 -0700

    Publish 1.0.0

--- bin/replay.js
+++ bin/replay.js
@@ -11,10 +11,48 @@ const argv = require('yargs')
   .describe('output', 'Specify output file (default is "./bin/repl.js")')
   .argv
 
+const fs = require('fs')
 const path = require('path')
+const findUp = require('find-up')
 
-var output = path.join(__dirname, argv.output || './bin/repl.js')
+// The root folder of this package
+const PACKAGE_ROOT = path.dirname(findUp.sync('package.json'))
 
-console.log(`Output file: ${output}`)
+// The package.json file of the consumer of this package
+const pkgPath = findUp.sync('package.json', { cwd: PACKAGE_ROOT })
+const pkg = require(pkgPath)
 
-process.exit(0)
+// The root folder of the consumer of this package
+const ROOT = path.dirname(pkgPath)
+
+var output = path.join(ROOT, argv.output || './bin/repl.js')
+
+const deps = []
+
+for (const dep in pkg.dependencies) {
+  deps.push(dep)
+}
+
+const replText =`
+const deps = ['${deps.join("','")}']
+
+const repl = require('repl').start()
+
+function load (dep) {
+  repl.context[dep] = require(dep)
+}
+
+function unload (dep) {
+  delete repl.context[dep]
+}
+
+repl.context.deps = deps
+repl.context.load = load
+repl.context.unload = unload
+repl.context.exit = process.exit
+repl.context.quit = process.exit
+`
+
+fs.writeFile(output, replText, (error) => {
+  if (error) { throw error }
+})
