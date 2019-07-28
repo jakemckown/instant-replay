2256f35de7826a1696d1266b0a3ebd11431ccbc9
Jake McKown <jbmckown@gmail.com>
2019-07-27 17:47:12 -0700

    Simplify options

--- bin/replay.js
+++ bin/replay.js
@@ -1,49 +1,20 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
-const path = require('path')
-
 const argv = require('yargs')
   .alias({
     'v': 'version',
     'h': 'help',
     'o': 'output'
-  }).argv
+  })
+  .boolean(['version', 'help'])
+  .string('output')
+  .describe('output', 'Specify output file (default is "./bin/repl.js")')
+  .argv
 
-var output = './bin/repl.js'
+const path = require('path')
 
-if (argv.version) {
-  version()
-} else if (argv.help) {
-  help()
-} else {
-  if (argv.output) {
-    output = argv.output
-  }
-  // do the rest
-  // const pkg = require(path.join(__dirname, 'package.json'))
-  process.exit(0)
-}
+var output = path.join(__dirname, argv.output || './bin/repl.js')
 
-function version () {
-  const replayPkg = require(path.join(__dirname, '../package.json'))
-  console.log(replayPkg && replayPkg.version)
-  process.exit(0)
-}
+console.log(`Output file: ${output}`)
 
-function help () {
-  console.log('instant-replay')
-  console.log('https://github.com/jakemckown/instant-replay')
-  console.log()
-  console.log('Instantly build a REPL playground from your dependencies')
-  console.log()
-  console.log('Usage:')
-  console.log('    replay [options]')
-  console.log()
-  console.log('Options:')
-  console.log('    -v, --version    Print version')
-  console.log('    -h, --help       Print command line options')
-  console.log('    -o, --output     Specify output file (default is "./bin/repl.js"')
-  console.log()
-  process.exit(0)
-}
+process.exit(0)
